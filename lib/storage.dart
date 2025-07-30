f26f254606b9dfd7af2ead6df6649011a8863729
Richoszxz <richoferdinand1106@gmail.com>
2025-07-30 11:49:59 +0700

    edit gemoy

--- lib/storage.dart
+++ lib/storage.dart
@@ -42,28 +42,3 @@ Future<List<Task>> loadTasks() async { // memuat data tugas
     return [];
   }
 }
-
-// KODE COBA COBA
-
-// import 'dart:io';
-// import 'dart:convert';
-// import 'task.dart';
-
-// const String fileName = 'tasks.json';
-
-// Future<List<Task>> loadTasks() async {
-//   final file = File(fileName);
-//   if (!file.existsSync()) {
-//     file.writeAsStringSync('[]');
-//   }
-
-//   final content = await file.readAsString();
-//   final List<dynamic> jsonData = json.decode(content);
-//   return jsonData.map((item) => Task.fromJson(item)).toList();
-// }
-
-// Future<void> saveTasks(List<Task> tasks) async {
-//   final file = File(fileName);
-//   final jsonData = tasks.map((task) => task.toJson()).toList();
-//   await file.writeAsString(json.encode(jsonData), flush: true);
-// }
